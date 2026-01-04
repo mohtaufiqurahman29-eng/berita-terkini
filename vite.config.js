6e489fa31f34e94ba8234c73a4db8507cf67e95f
topik <mohtaufiqurahman29@gmail.com>
2026-01-04 11:11:10 +0700

    commit 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineconfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineconfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://example.com"
+    }
+  }
 })
